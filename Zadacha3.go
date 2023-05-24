57489d37e7687b828101a2e05ce2527bae67ed80
Dmitrii <115589870+DmitriiArkatov@users.noreply.github.com>
2023-05-24 20:35:37 +0400

    Update Zadacha3.go
    bugs fixed

--- Zadacha3.go
+++ Zadacha3.go
@@ -8,47 +8,52 @@ import (
 	"time"
 )
 
-// main - многопоточный генератор случайных чисел
+// main - многопоточный генератор уникальных чисел
 func main() {
+	ch := make(chan int)
+	trechmap := make(map[int]int)
 	var uniquenum []int
 	start := time.Now()
 	var wg sync.WaitGroup
-	limit, flow, count := options()
-	for len(uniquenum) != count {
-		for i := 0; i <= flow; i++ {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				r := rand.Intn(limit + 1)
-				bo := uniquenessCheck(uniquenum, r)
-				if bo != true {
-					uniquenum = append(uniquenum, r)
-				} else {
-				}
-			}()
-		}
-		wg.Wait()
+
+	limit, flow, count := oPtions()
+
+	for i := 0; i < flow; i++ {
+		wg.Add(1)
+		go randomaizer(&wg, ch, limit, count)
 	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for r := range ch {
+		if r != trechmap[r] {
+			fmt.Println(r, trechmap[r])
+			uniquenum = append(uniquenum, r)
+			trechmap[r] = r
+			fmt.Println(uniquenum)
+		}
+	}
+
 	fmt.Println(uniquenum)
 	duration := time.Since(start)
 	fmt.Printf("Время исполнения: %s\n", duration)
 }
 
 // Ввод аргументов с консоли
-func options() (int, int, int) {
-	limit := flag.Int("limit", 0, "Limiting number generation")
-	flow := flag.Int("flow", 0, "number of threads")
-	count := flag.Int("count", 0, "number of unique numbers")
+func oPtions() (int, int, int) {
+	limit := flag.Int("limit", 15, "Limiting number generation")
+	flow := flag.Int("flow", 1, "number of threads")
+	count := flag.Int("count", 5, "number of unique numbers")
 	flag.Parse()
 	return *limit, *flow, *count
 }
-
-// uniquenessCheck - проверяет наличие числа в списке
-func uniquenessCheck(uniquenum []int, x int) bool {
-	for _, n := range uniquenum {
-		if x == n {
-			return true
-		}
+func randomaizer(wg *sync.WaitGroup, ch chan int, limit int, count int) {
+	defer wg.Done()
+	for i := 1; i <= 1000; i++ {
+		r := rand.Intn(limit + 1)
+		ch <- r
 	}
-	return false
 }
