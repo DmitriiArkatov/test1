1f0600cbe112ed194fec6080ba031bd26972d54c
Dmitrii <115589870+DmitriiArkatov@users.noreply.github.com>
2023-05-26 19:30:48 +0400

    Update Zadacha4(web).go

--- Zadacha4(web).go
+++ Zadacha4(web).go
@@ -1,103 +1,48 @@
 package main
 
 import (
-	"bufio"
-	"flag"
 	"fmt"
-	"io"
+	"html/template"
 	"net/http"
-	"os"
+	"packet"
 	"strconv"
-	"sync"
-	"time"
 )
 
-// main - многопоточно парсит по этим ссылкам HTML-страницы  и создает локальные серверы
+
+
 func main() {
-	start := time.Now()
-	var wg sync.WaitGroup
-	filepath := arguments()
-	links, err := readSources(filepath)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for i, link := range links {
-		i += 1234
-		wg.Add(1)
-		go parser(link, &wg, i) //каждая горутина забирает HTML со страницы и отправляет его на наш сервер
-		fmt.Printf("http://localhost:%d\n", i)
-	}
-	wg.Wait()
-	duration := time.Since(start)
-	fmt.Printf("Время исполнения: %s\n", duration)
+	createLocalhost()
 }
 
-// arguments - считываем аргументы с консоли
-func arguments() string {
-	filepath := flag.String("pathfile", "/home/vitaliy/awesomeProject/zadone/link.txt", "the path to the text file to be scanned ") // переменная для считывания файла
-	flag.Parse()
-	return *filepath
-}
-
-// readSources - читает файл и заносит все в срез
-func readSources(argORC string) ([]string, error) {
-	var links []string
-	file, err := os.Open(argORC) //путь до файла
+func local(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("/home/vitaliy/awesomeProject/inputBox.html")
 	if err != nil {
-		return nil, err
+		fmt.Fprintf(w, err.Error())
 	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			fmt.Println(err)
-			return
+	err = t.ExecuteTemplate(w, "button", nil)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	if r.Method == "POST" {
+		limit, _ := strconv.Atoi(fmt.Sprintf(r.FormValue("limit")))
+		flow, _ := strconv.Atoi(fmt.Sprintf(r.FormValue("flow")))
+		count, _ := strconv.Atoi(fmt.Sprintf(r.FormValue("count")))
+
+		if limit == 0 || count == 0 {
+			fmt.Fprintf(w, "Please enter the correct data")
 		}
-	}(file)
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		links = append(links, scanner.Text())
+		uniquenum := Zadacha3.Random(limit, flow, count)
+		fmt.Fprintf(w, "%d", uniquenum)
+
 	}
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
-	fmt.Println(len(links))
-	fmt.Println(links)
-	return links, nil
 }
 
-// получаем HTML страницы
-func parser(link string, wg *sync.WaitGroup, i int) {
-	resp, err := http.Get(link)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-	}(resp.Body)
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	text := string(body)
-	createLocalhost(text, i, wg)
-}
-
-// создание сервера на который отправляем HTML
-func createLocalhost(text string, por int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	port := strconv.Itoa(por)
+func createLocalhost() {
+	port := "8888"
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, text)
-	})
+	mux.HandleFunc("/", local)
 
 	err := http.ListenAndServe(":"+port, mux)
 	if err != nil {
