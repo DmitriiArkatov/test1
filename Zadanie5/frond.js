b915d52e7cd0c96475e564b9f73911ee08c2db6d
Dmitrii <115589870+DmitriiArkatov@users.noreply.github.com>
2023-06-01 20:01:36 +0400

    Update frond.js

--- Zadanie5/frond.js
+++ Zadanie5/frond.js
@@ -1,22 +1,29 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById("btn").addEventListener("click", () => {
+        // привязываем переменные к элементам DOM
         let limit = document.getElementById("limit").value;
         let flow = document.getElementById("flow").value;
         let count = document.getElementById("count").value;
         document.getElementById("result").innerHTML = "";
-
-        let url = `ws://192.168.81.53:8888/ws?limit=${limit}&flow=${flow}&count=${count}`;
+        // Извлекаем базовую часть URL (хост и порт)
+        let host = window.location.host;
+        // Создаем URL, объединяя базовую часть и относительный путь
+        let url = `ws://${host}/ws?limit=${limit}&flow=${flow}&count=${count}`;
         let socket = new WebSocket(url);
+        //открываем соединение
         socket.onopen = () => {
             socket.send(url)
             console.log("Socket open connection!");
         };
+        //получаем ответ от сервера и выводим его на экран
         socket.onmessage = event => {
             document.getElementById('result').innerHTML += event.data+"  ";
         }
+        //закрываем соединение
         socket.onclose = event => {
             console.log("Socket closed connection: ", event);
         };
+        //ошибка соединения
         socket.onerror = error => {
             console.log("Socket error: ", error);
         };
