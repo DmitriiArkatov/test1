862bb036819b4b3f92423cc442e8548bd17cbf5d
Dmitrii <115589870+DmitriiArkatov@users.noreply.github.com>
2023-06-01 20:01:13 +0400

    Update zadanie5.go

--- Zadanie5/zadanie5.go
+++ Zadanie5/zadanie5.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"gorilla/websocket"
 	"log"
 	"math/rand"
@@ -13,7 +12,7 @@ import (
 
 func main() {
 	createLocalhost()
-	log.Fatal(http.ListenAndServe("192.168.81.53:8888", nil))
+	log.Fatal(http.ListenAndServe(":"+"8888", nil))
 }
 
 // createLocalhost - создает локальные сервера
@@ -25,7 +24,7 @@ func createLocalhost() {
 
 // local - сервер с соединение HTTP
 func local(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "/home/vitaliy/awesomeProject/trening/response.html")
+	http.ServeFile(w, r, "/home/vitaliy/awesomeProject/trening/zadanie5.html")
 }
 
 // localjs - выгружаем JS файл
@@ -41,6 +40,10 @@ func wsLocal(w http.ResponseWriter, r *http.Request) {
 		WriteBufferSize: 1024,
 	}
 
+	//забираем из url данные
+	limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
+	flow, _ := strconv.Atoi(r.URL.Query().Get("flow"))
+	count, _ := strconv.Atoi(r.URL.Query().Get("count"))
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true } //Проверяем входящий запрос на подключение по WebSocket
 
 	conn, err := upgrader.Upgrade(w, r, nil) //Модернизируем наше HTTP соединение и подключаемся по WebSocket
@@ -58,58 +61,44 @@ func wsLocal(w http.ResponseWriter, r *http.Request) {
 	}(conn)
 
 	log.Println("Client connected")
-
-	reader(conn, r)
+	
+	randomAnswer(conn, limit, flow, count)
 }
 
-// reader - читаем сообщение по URL и WebSocket
-func reader(conn *websocket.Conn, r *http.Request) {
+// randomAnswer - создаем уникальный ответ перед отправкой
+func randomAnswer(conn *websocket.Conn, limit int, flow int, count int) {
 	ch := make(chan int)
 	var wg sync.WaitGroup
-	for {
-		_, p, err := conn.ReadMessage() // читаем сообщения
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		fmt.Println(p)
+	defer wg.Wait()
 
-		//забираем из url данные
-		limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
-		flow, _ := strconv.Atoi(r.URL.Query().Get("flow"))
-		count, _ := strconv.Atoi(r.URL.Query().Get("count"))
-
-		//если условие - истина , то начинается запись уникального числа в канал, для отправки
-		if limit > 0 && flow > 0 && count > 0 {
-			go checkSendAnswer(conn, ch, count)
-			func() {
-				for i := 0; i < flow; i++ {
-					//запускается указаное кол-во горутин
-					wg.Add(1)
-					go func() {
-						defer wg.Done()
-						defer close(ch)
-						for i := -1; i <= 100; i++ {
-							r := rand.Intn(limit + 1)
-							ch <- r
-						}
-					}()
+	//если условие - истина , то начинается запись уникального числа в канал, для отправки
+	if limit > 0 && flow > 0 && count > 0 {
+		go checkSendAnswer(conn, ch, count, &wg)
+		for i := 0; i < flow; i++ {
+			//запускается указаное кол-во горутин
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for i := -1; i <= 100; i++ {
+					r := rand.Intn(limit + 1)
+					ch <- r
 				}
 			}()
-		} else {
-			answer := "Все параметры должны быть > 0!!!"
-			if err := conn.WriteMessage(websocket.TextMessage, []byte(answer)); err != nil {
-				log.Println(err)
-				return
-			}
+		}
+	} else {
+		answer := "Все параметры должны быть > 0!!!"
+		if err := conn.WriteMessage(websocket.TextMessage, []byte(answer)); err != nil {
+			log.Println(err)
+			return
 		}
 	}
 }
 
 // checkSendAnswer - проверяем на уникальность и отсылаем ответ пользователю по WebSocket
-func checkSendAnswer(conn *websocket.Conn, ch <-chan int, count int) {
+func checkSendAnswer(conn *websocket.Conn, ch chan int, count int, wg *sync.WaitGroup) {
 	trashmap := make(map[int]int)
-
+	defer close(ch)
+	defer wg.Wait()
 	//проверка уникальности числа в списке
 	for r := range ch {
 		if r != trashmap[r] {
